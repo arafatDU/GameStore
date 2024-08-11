0d7cac6ace96cc23261e722aadfbc61f8253342c
ARAFAT <munshiarafathussain@gmail.com>
2024-08-11 19:24:06 +0600

    Connect with SQLite

--- GameStore.Api/Dtos/GameDetailsDto.cs
+++ GameStore.Api/Dtos/GameDetailsDto.cs
@@ -1,9 +1,8 @@
 namespace GameStore.Api.Dtos;
 
-public record class GameDto(
+public record class GameDetailsDto(
     int Id, 
     string Name, 
     int GenreId, 
     decimal Price,
-    DateOnly ReleaseDate
-);
+    DateOnly ReleaseDate);
